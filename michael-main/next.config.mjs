f92fe92dc3996e1bab52ab23e53839381eae7792
michaeltran5 <mttran7@wisc.edu>
2024-10-23 21:14:51 -0500

    trying to deploy

--- michael-main/next.config.mjs
+++ michael-main/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    basePath: '/michael-main',
+    assetPrefix: '/michael-main',
+    trailingSlash: true,
+    distDir: 'dist',   // Optional: specifies build output directory
+    reactStrictMode: true,  // Good practice to keep this enabled
+};
 
 export default nextConfig;
